c10ac6fcc22d917f18b102b64a4bcc8a2473f88b
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-05-28 09:42:23 +0300

    add new inventory routs

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -1,7 +1,16 @@
 import { getInventory } from "@/features/inventory/api/get-inventory";
 
-export async function GET() {
-  const data = await getInventory();
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const city = searchParams.get("city") || undefined;
+
+  const lowStock = searchParams.get("lowStock") === "true";
+
+  const data = await getInventory({
+    city,
+    lowStock,
+  });
 
   return Response.json(data);
 }
--- features/inventory/api/get-inventory.ts
+++ features/inventory/api/get-inventory.ts
@@ -1,5 +1,7 @@
+import { InventoryFilters } from "../types";
+
 import { getInventoryQuery } from "../queries/get-inventory-query";
 
-export async function getInventory() {
-  return getInventoryQuery();
+export async function getInventory(filters?: InventoryFilters) {
+  return getInventoryQuery(filters);
 }
