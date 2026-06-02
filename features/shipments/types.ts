f9e6f86c3a27dd874bf36207d70968ea014d953b
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-06-02 14:02:22 +0300

    added new shipment types

--- features/shipments/types.ts
+++ features/shipments/types.ts
@@ -19,3 +19,33 @@ export type ShipmentItem = {
 
   quantity: number;
 };
+
+export type ShipmentDetailsItem = {
+  product_id: string;
+  name: string;
+  sku: string;
+  quantity: number;
+};
+
+export type ShipmentDetails = {
+  shipment_id: string;
+
+  status: Shipment["status"];
+
+  created_at: string;
+  completed_at: string | null;
+
+  source_store: {
+    id: string;
+    name: string;
+    city: string;
+  };
+
+  destination_store: {
+    id: string;
+    name: string;
+    city: string;
+  };
+
+  items: ShipmentDetailsItem[];
+};
