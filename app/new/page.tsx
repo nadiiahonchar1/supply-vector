3256bf601afca6a2731649aa80f43de2a24c7109
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-06-09 15:29:41 +0300

    added mock products

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,12 +1,19 @@
 import { getStoresQuery } from "@/features/stores/queries/get-stores-query";
-import { getProductsQuery } from "@/features/products/queries/get-products-query";
+// import { getProductsQuery } from "@/features/products/queries/get-products-query";
 import { CreateShipmentForm } from "@/features/shipments/components/shipment-form";
 
+const products = [
+  { id: "p1", name: "iPhone 15", sku: "APL-IP15" },
+  { id: "p2", name: "Samsung S24", sku: "SMS-S24" },
+];
+
 export default async function Page() {
-  const [stores, products] = await Promise.all([
-    getStoresQuery(),
-    getProductsQuery(),
-  ]);
+  //   const [stores, products] = await Promise.all([
+  //     getStoresQuery(),
+  //     getProductsQuery(),
+  //   ]);
+
+  const [stores] = await Promise.all([getStoresQuery()]);
 
   return (
     <div style={{ padding: 20 }}>
