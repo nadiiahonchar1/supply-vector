b614227520d695b5ca9c453cd03a532a3eab7ebe
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-06-15 21:22:08 +0300

    added users tables

--- db/schema.sql
+++ db/schema.sql
@@ -60,3 +60,39 @@ CREATE TABLE shipment_items (
 
   quantity INTEGER NOT NULL CHECK (quantity > 0)
 );
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+
+  is_active BOOLEAN NOT NULL DEFAULT TRUE,
+
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE roles (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  code TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE user_roles (
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  role_id UUID NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+
+  PRIMARY KEY(user_id, role_id)
+);
+
+CREATE TABLE user_stores (
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  store_id UUID NOT NULL REFERENCES stores(id) ON DELETE CASCADE,
+
+  PRIMARY KEY(user_id, store_id)
+);
