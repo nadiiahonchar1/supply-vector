eaab32f1531c6ea7a2f27c94f67131a9466156be
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-07-21 13:26:37 +0300

    fix: get-current-user with cache

--- lib/auth/get-current-user.ts
+++ lib/auth/get-current-user.ts
@@ -1,29 +1,30 @@
+import { cache } from "react";
 import { sql } from "@/db";
 import { getSessionFromCookie } from "./session";
-import type { CurrentUser} from "@/features/auth/types";
+import type { CurrentUser } from "@/features/auth/types";
 
-export async function getCurrentUser(): Promise<CurrentUser | null> {
+export const getCurrentUser = cache(async (): Promise<CurrentUser | null> => {
   const session = await getSessionFromCookie();
 
   if (!session) return null;
 
   const users = (await sql`
-    SELECT
-      u.id,
-      u.email,
-      u.first_name,
-      u.last_name,
-      u.is_active,
-      r.code AS role
-    FROM users u
-    JOIN user_roles ur
-      ON ur.user_id = u.id
-    JOIN roles r
-      ON r.id = ur.role_id
-    WHERE u.id = ${session.user_id}
-      AND u.is_active = true
-    LIMIT 1
-  `) as CurrentUser[];
+      SELECT
+        u.id,
+        u.email,
+        u.first_name,
+        u.last_name,
+        u.is_active,
+        r.code AS role
+      FROM users u
+      JOIN user_roles ur
+        ON ur.user_id = u.id
+      JOIN roles r
+        ON r.id = ur.role_id
+      WHERE u.id = ${session.user_id}
+        AND u.is_active = true
+      LIMIT 1
+    `) as CurrentUser[];
 
   return users[0] ?? null;
-}
+});
