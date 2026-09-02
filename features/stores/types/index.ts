ad2e5d1363c021a5a87decb39cf192b25a836050
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-09-02 10:19:17 +0300

    added sore type

--- features/stores/types/index.ts
+++ features/stores/types/index.ts
@@ -1,5 +1,32 @@
-export type StoreOption = {
+export type Store = {
   id: string;
   name: string;
   city: string;
+  address: string;
+
+  latitude: number | null;
+  longitude: number | null;
+
+  is_storage_node: boolean;
+  max_capacity: number | null;
+
+  created_by: string | null;
+  updated_by: string | null;
+
+  created_at: string;
+  updated_at: string;
 };
+
+export type CreateStoreInput = {
+  name: string;
+  city: string;
+  address: string;
+
+  latitude?: number | null;
+  longitude?: number | null;
+
+  is_storage_node?: boolean;
+  max_capacity?: number | null;
+};
+
+export type UpdateStoreInput = Partial<CreateStoreInput>;
