c180b65d1a75c7ab16ab5167934ebcfeaa78af58
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-09-07 11:45:12 +0300

    added vehicles route

---
+++ app/api/vehicles/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import { VehiclesService } from "@/lib/vehicles/vehicles.service";
+import { handleApiError } from "@/lib/errors/handle-api-error";
+
+import { createVehicleSchema } from "@/features/vehicles/validation";
+
+export async function GET() {
+  try {
+    const vehicles = await VehiclesService.getVehicles();
+
+    return NextResponse.json(vehicles);
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const data = createVehicleSchema.parse(body);
+
+    const vehicle = await VehiclesService.createVehicle(data);
+
+    return NextResponse.json(vehicle, { status: 201 });
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
