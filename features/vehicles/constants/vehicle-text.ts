b0bda39331348827f3101990e226827812e1c919
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2026-09-07 11:54:51 +0300

    added vehicle text

---
+++ features/vehicles/constants/vehicle-text.ts
@@ -0,0 +1,13 @@
+export const VEHICLE_TEXT = {
+  error: {
+    empty_vehicle: "Машину не знайдено",
+    forbidden_create: "Недостатньо прав для створення машини",
+    forbidden_update: "Недостатньо прав для редагування машини",
+    invalid_availability: "Некоректний період доступності машини",
+  },
+
+  success: {
+    created: "Машину успішно створено",
+    updated: "Машину успішно оновлено",
+  },
+} as const;
